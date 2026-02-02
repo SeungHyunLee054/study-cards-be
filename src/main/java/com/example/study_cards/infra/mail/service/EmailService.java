9400f59813b8824cd4e13896f87586482bf69ecb
seunghyun <seunghyun937@gmail.com>
2026-02-02 18:35:43 +0900

    Feat: 인프라 서비스 추가
    - EmailService 추가 (비밀번호 리셋 이메일 발송)
    - PasswordResetCodeService 추가 (Redis 기반 코드 관리)
    - RateLimitService 추가 (API 요청 제한)
    - AsyncConfig 추가 (비동기 처리 설정)

---
+++ src/main/java/com/example/study_cards/infra/mail/service/EmailService.java
@@ -0,0 +1,37 @@
+package com.example.study_cards.infra.mail.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+// TODO: 메일 서비스 활성화 시 @RequiredArgsConstructor, @Service 주석 해제
+@RequiredArgsConstructor
+//@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Async
+    public void sendPasswordResetCode(String to, String code) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("[Study Cards] 비밀번호 재설정 인증 코드");
+        message.setText(String.format("""
+                안녕하세요.
+
+                비밀번호 재설정을 위한 인증 코드입니다.
+
+                인증 코드: %s
+
+                이 코드는 5분간 유효합니다.
+                본인이 요청하지 않았다면 이 이메일을 무시해주세요.
+
+                감사합니다.
+                Study Cards 팀
+                """, code));
+
+        mailSender.send(message);
+    }
+}
